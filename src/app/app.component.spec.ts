164ae8eadddfb883b991b865d07b41446666f00e
joshdlcruz-zipeli <joshua.delacruz@zipeli.com>
2021-09-10 15:08:55 +0800

    fixed name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-cop'`, () => {
+  it(`should have as title 'angular-poc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-cop');
+    expect(app.title).toEqual('angular-poc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-cop app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'angular-poc app is running!'
+    );
   });
 });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   config: {
     apiKey: 'AIzaSyA43-9vgSQi5oBUUknilJ7ZQZ5EVZpc6zw',
-    authDomain: 'angular-cop.firebaseapp.com',
-    projectId: 'angular-cop',
-    storageBucket: 'angular-cop.appspot.com',
+    authDomain: 'angular-poc.firebaseapp.com',
+    projectId: 'angular-poc',
+    storageBucket: 'angular-poc.appspot.com',
     messagingSenderId: '540452092943',
     appId: '1:540452092943:web:913ec73a7a8d0eea3e377a',
     measurementId: 'G-7SEHZS1ECF',
