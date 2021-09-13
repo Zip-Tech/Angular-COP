a8bd3f943e6e84f7c2d5bd1426346169e10f73db
joshdlcruz-zipeli <joshua.delacruz@zipeli.com>
2021-09-13 09:56:13 +0800

    updated auth forms

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/state/auth/auth.model';
+import { StoreRootState } from 'src/app/state/state.reducers';
+
+import * as fromAuth from '../../state/auth/auth.selector';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   title = 'angular-poc';
+  user: any;
 
-  constructor(public auth: AuthService) {}
+  constructor(private store: Store<StoreRootState>, public auth: AuthService) {}
+
+  ngOnInit() {
+    this.store.select(fromAuth.selectUser).subscribe((user) => {
+      if (user) {
+        this.user = user;
+      }
+    });
+  }
 
   logout() {
     this.auth.signOut();
